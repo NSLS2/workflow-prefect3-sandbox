18218fdf1666a279f76a615f937d7035ea3742e6
Jun Aishima <jaishima@bnl.gov>
2025-06-23 17:11:24 -0400

    run pre-commit

--- pkg_info.py
+++ pkg_info.py
@@ -3,4 +3,11 @@ import os
 import time
 
 for dist in distributions():
-    print("%s %s: %s" % (dist.metadata["Name"], dist.version, time.ctime(os.path.getctime(dist._path))))
+    print(
+        "%s %s: %s"
+        % (
+            dist.metadata["Name"],
+            dist.version,
+            time.ctime(os.path.getctime(dist._path)),
+        )
+    )
