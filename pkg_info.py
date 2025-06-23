e15ddfe38e0499f8aa98961b287ed6229dfde939
Jun Aishima <jaishima@bnl.gov>
2025-06-23 17:07:49 -0400

    fix ruff issue

--- pkg_info.py
+++ pkg_info.py
@@ -1,5 +1,6 @@
 from importlib.metadata import distributions
-import os, time
+import os
+import time
 
 for dist in distributions():
     print("%s %s: %s" % (dist.metadata["Name"], dist.version, time.ctime(os.path.getctime(dist._path))))
