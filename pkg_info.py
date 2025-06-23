d1f6299c51dcad7eec8cdb34774c7e589b53e0c0
Jun Aishima <jaishima@bnl.gov>
2025-06-23 17:26:54 -0400

    pre-commit fixes

--- pkg_info.py
+++ pkg_info.py
@@ -1,13 +1,10 @@
-from importlib.metadata import distributions
+from __future__ import annotations
+
 import os
 import time
+from importlib.metadata import distributions
 
 for dist in distributions():
-    print(
-        "%s %s: %s"
-        % (
-            dist.metadata["Name"],
-            dist.version,
-            time.ctime(os.path.getctime(dist._path)),
-        )
+    print(  # noqa: T201
+        f"{dist.metadata['Name']} {dist.version}: {time.ctime(os.path.getctime(dist._path))}"  # noqa: PTH205
     )
